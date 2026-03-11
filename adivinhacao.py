d52dc9daf8f25047824e2a7b702b8827bd8561ab
Larissadods <larissa.pereira.araujo@escola.pr.gov.br>
2026-03-11 11:50:50 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,12 +2,12 @@ import random
 
 def jogar():
 
-    print("--------------------------------------")
-    print("Bem vindo ao meu jogo de adivinhação!")
-    print("--------------------------------------")
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-    numero_secreto = random.randrange(1, 101)
-    total_tentativas = 0
+    numero_secreto = random.randrange(1,101)
+    total_de_tentativas = 0
     pontos = 1000
 
     print("Qual nível de dificuldade?")
@@ -16,17 +16,17 @@ def jogar():
     nivel = int(input("Defina o nível: "))
 
     if(nivel == 1):
-        total_tentativas = 20
+        total_de_tentativas = 20
     elif(nivel == 2):
-        total_tentativas = 10
+        total_de_tentativas = 10
     else:
-        total_tentativas = 5
+        total_de_tentativas = 5
 
+    for rodada in range(1, total_de_tentativas + 1):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-    for rodada in range (1, total_tentativas + 1):
-        print("Tentativa {} de {}".format(rodada, total_tentativas))
         chute_str = input("Digite um número entre 1 e 100: ")
-        print("Você digitou ", chute_str)
+        print("Você digitou " , chute_str)
         chute = int(chute_str)
 
         if(chute < 1 or chute > 100):
@@ -42,13 +42,13 @@ def jogar():
             break
         else:
             if(maior):
-                print("Você errou: O seu chute foi maior que o número secreto.")
+                print("Você errou! O seu chute foi maior do que o número secreto.")
             elif(menor):
-                print("Você errou: O seu chute foi menor que o número secreto.")
+                print("Você errou! O seu chute foi menor do que o número secreto.")
             pontos_perdidos = abs(numero_secreto - chute)
             pontos = pontos - pontos_perdidos
 
-    print("Fim do Jogo")
-    
+    print("Fim do jogo")
+
 if(__name__ == "__main__"):
     jogar()
